c6cd2473efeb6dbf989e08142112e96ea070c620
viktor-mazepa <viktor.mazepa@gmail.com>
2023-12-14 20:14:01 +0200

    Update terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 module "flux_bootstrap" {
-  source            = "./modules/fluxcd-flux-bootstrap"
+  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap"
   github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
   private_key       = module.tls_private_key.private_key_pem
-  config_path       = module.gke_cluster.kubeconfig
+  config_path       = module.kind_cluster.kubeconfig
   github_token      = var.GITHUB_TOKEN
 }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -28,23 +28,23 @@ variable "GKE_POOL_NAME" {
 }
 
 variable "GITHUB_OWNER" {
-    type = string
-    default = "viktor-mazepa"
-    description = "GitHub user"
+  type        = string
+  default     = "viktor-mazepa"
+  description = "GitHub user"
 }
 
 variable "FLUX_GITHUB_REPO" {
-    type = string
-    default = "kbot"
-    description = "The name of repository to store Flux manifest"
+  type        = string
+  default     = "kbot"
+  description = "The name of repository to store Flux manifest"
 }
 variable "GITHUB_TOKEN" {
-    type = string
-    description = "GitHub personal access token"
+  type        = string
+  description = "GitHub personal access token"
 }
 
-variable "FLUX_GITHUB_TARGET_PATH"{
-    type = string
-    default = "clusters"
-    description = " Flux manifets subbdirectory"
+variable "FLUX_GITHUB_TARGET_PATH" {
+  type        = string
+  default     = "clusters"
+  description = " Flux manifets subbdirectory"
 }
