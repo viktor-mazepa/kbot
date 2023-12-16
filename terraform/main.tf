a02b951ffa27b1fcaa7d7669f6f7872c9f8379af
viktor-mazepa <viktor.mazepa@gmail.com>
2023-12-16 23:05:06 +0000

    Change terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,18 @@
-module "flux_bootstrap" {
-  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap"
-  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
-  private_key       = module.tls_private_key.private_key_pem
-  config_path       = module.gke_cluster.kubeconfig
-  github_token      = var.GITHUB_TOKEN
+provider "flux" {
+  kubernetes = {
+    config_path = "${path.cwd}/.terraform/modules/gke_cluster/kubeconfig"
+  }
+  git = {
+    url  = "ssh://git@github.com/${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}.git"
+    ssh = {
+      username    = "git"
+      private_key = module.tls_private_key.private_key_pem
+    }
+  }
+}
+
+resource "flux_bootstrap_git" "this" {
+  path = "./clusters"
 }
 
 module "github_repository" {
@@ -19,6 +28,7 @@ module "gke_cluster" {
   source         = "github.com/viktor-mazepa/tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GKE_CLUSTER_NAME = var.GKE_CLUSTER_NAME
   GKE_NUM_NODES  = 1
 }
 
@@ -26,6 +36,12 @@ terraform {
   backend "gcs" {
     bucket = "kbot_bucket"
     prefix = "terraform/state"
+  }
+  required_providers {
+    flux = {
+      source = "fluxcd/flux"
+      version = "1.2.1"
+    }
   }
 }
 
